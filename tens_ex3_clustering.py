b668f6405825ca0ab81bd502ab8dc4a16ce36ff7
markok-pub <77977666+markok-pub@users.noreply.github.com>
2021-01-25 16:11:03 +0100

    Delete tens_ex3_clustering.py

--- tens_ex3_clustering.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from IPython.display import clear_output
-from six.moves import urllib
-import logging
-
-
